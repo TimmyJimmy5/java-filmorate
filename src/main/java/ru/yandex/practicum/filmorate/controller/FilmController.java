7084a823aca2335cde45830d581ef6676896b7fa
Timmy Jimmy <stalgorn163@gmail.com>
2024-08-27 16:12:34 +0300

    Реализовано ТЗ №10 в полном объеме. Тесты для проверки работы валидации прописаны. АПИ протестировано в постмане в полном объеме.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -19,7 +19,7 @@ import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
+    private final Logger log = LoggerFactory.getLogger(FilmController.class);
     private final Map<Long, Film> films = new HashMap<>();
     private final static LocalDate FIRST_FILM_DATE = LocalDate.of(1895, 12, 28);
     private final static int MAX_NAME_LENGTH = 200;
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
     private final Map<Long, User> users = new HashMap<>();
 
     @GetMapping
