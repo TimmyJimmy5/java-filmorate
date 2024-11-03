a3ef2260e28b1c0be5a9ca0c01fed52036a595c7
Alexander <165676758+TimmyJimmy5@users.noreply.github.com>
2024-11-04 00:39:27 +0300

    Add database (#6)
    * Промежуточный коммит для сохранения уже написанного в облаке под ТЗ №12.
    
    * Промежуточный коммит для сохранения уже написанного в облаке под ТЗ №12.
    Реализованы сервисы, кроме юзерского, добавлены мапперы, в некоторых местах причёсан код, реализованы контроллеры, кроме юзерского.
    Предстоит еще: доделать юзерскую часть программы,
    убрать старые блоки кода, убрать старые классы, разобраться с тестами.
    
    * Промежуточный коммит для сохранения уже написанного в облаке под ТЗ №12.
    Реализовано всё, но нет тестов. Не работает добавление в друзья, не работает получение списка фильмов.
    
    * Промежуточный коммит для сохранения уже написанного в облаке под ТЗ №12.
    Все тесты постмана пройдены.
    Реализовано всё, но нет тестов.
    
    * Эстетические правки кода.
    
    * Наименование компонентов.
    
    * Закомментирование файла теста.
    
    * Корректировка имён
    
    * Получение юзера по айди восстановлено
    
    * Получение юзера по айди восстановлено

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,8 +1,9 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,41 +11,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.dto.user.UserDto;
+import ru.yandex.practicum.filmorate.dto.user.UserRequest;
 import ru.yandex.practicum.filmorate.service.UserService;
-
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping
-    public Collection<User> findAll() {
+    public Collection<UserDto> findAll() {
         return userService.findAll();
     }
 
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable Long id) {
+        return userService.get(id);
+    }
+
     @PostMapping
-    public User create(@Valid @RequestBody User user) {
-        return userService.create(user);
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto create(@Valid @RequestBody UserRequest request) {
+        return userService.create(request);
     }
 
     @PutMapping
-    public User update(@Valid @RequestBody User newUser) {
-        return userService.update(newUser);
+    public UserDto update(@Valid @RequestBody UserRequest request) {
+        return userService.update(request);
     }
 
     @GetMapping("/{userId}/friends")
-    public Set<User> getFriends(@PathVariable Long userId) {
+    public List<UserDto> getFriends(@PathVariable Long userId) {
         return userService.getFriends(userId);
     }
 
@@ -59,7 +64,7 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/friends/common/{otherId}")
-    public Set<User> getCommonFriends(@PathVariable Long userId, @PathVariable Long otherId) {
+    public Set<UserDto> getCommonFriends(@PathVariable Long userId, @PathVariable Long otherId) {
         return userService.getCommonFriends(userId, otherId);
     }
 }
